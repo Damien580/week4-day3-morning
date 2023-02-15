6438c877b0e54ca840a45c75fc65d4542d9249c7
Damien Redd <damiens.redd@gmail.com>
2023-02-15 09:41:34 -0600

    working code

--- chal-balanced-parenthesis/balancedParens.js
+++ chal-balanced-parenthesis/balancedParens.js
@@ -6,3 +6,41 @@ let sample4 = "Balanced."
 
 // Write your solution below:
 
+const balanced = str =>{
+    let parent = true
+    let count = 0
+
+    for (i = 0; i < str.length; i++){
+        if (str[i] == '('){
+            count += 1
+        } else count -= 1 
+        if (count < 0){
+            parent = false
+        }
+    }
+    if (count != 0){
+        parent = false
+    }
+    return parent
+}
+
+console.log(balanced(')balanced('))
+
+console.log('================================')
+
+const balance = str =>{
+    let count = 0
+    
+    for(i = 0; i < str.length; i++){
+        if (str[i] === '('){
+           count++
+    }  else if (str[i] === ')')
+    count--
+}
+    if (count < 0){
+        return false
+    }
+    return count === 0
+}
+
+console.log(balance("(Oh Noes!)"))
